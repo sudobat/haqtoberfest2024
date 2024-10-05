d0cf1b6528743f340f955c7fd41cbbb856ab45ae
pau-torrente <ptbadia@gmail.com>
2024-10-05 14:27:45 +0200

    .

--- challenges/qubit_mapping/src/CircuitTranspiler.py
+++ challenges/qubit_mapping/src/CircuitTranspiler.py
@@ -1,8 +1,16 @@
 # We assume non-directed star topology and that the qubit 2 is the center of the star
 
+import qibo
+from qibo import gates, models
+
+
 class CircuitTranspiler:
     def __init__(self, circuit):
         self._circuit = circuit
+        self._centre = 2
+        self._qdict = {i: i for i in range(5)}
+        self._gatelist = [(i, i.name, i.qubits) for i in self._circuit.queue]
+        self._outputcirc = qibo.models.Circuit(5)
 
     def transpile(self):
         self._adapt()
@@ -10,8 +18,8 @@ class CircuitTranspiler:
         pass
 
     def _adapt(self):
+        # for gate, info in self._gatelist:
         pass
 
     def _optimize(self):
         pass
-
