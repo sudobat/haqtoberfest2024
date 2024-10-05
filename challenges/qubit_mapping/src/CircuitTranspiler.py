c58005b9c9bf90d97a77e1fc0be369067e9c54f0
pau-torrente <ptbadia@gmail.com>
2024-10-05 17:27:16 +0200

    Initial version of routing

--- challenges/qubit_mapping/src/CircuitTranspiler.py
+++ challenges/qubit_mapping/src/CircuitTranspiler.py
@@ -1,10 +1,19 @@
 # We assume non-directed star topology and that the qubit 2 is the center of the star
 from typing import List, Tuple, Dict
 
-from qibo import Circuit, models
+from networkx import astar_path
+from qibo import Circuit, models, gates
+from collections import deque
+import networkx as nx
 
 
 class CircuitTranspiler:
+    def __init__(self, circuit: models.Circuit, topology: dict[int, list[int]]):
+        self.circuit = circuit
+        self.topology_graph = nx.Graph(dict_topology_tolist(topology))
+        self.output_circuit = models.Circuit(self.circuit.nqubits)
+        self.mapping = {i: i for i in range(self.circuit.nqubits)}
+
     def transpile(self, circuit: models.Circuit) -> Circuit:
         timesteps = self.generate_timesteps(circuit)
         mapping = self.initial_mapping(timesteps)
@@ -39,7 +48,7 @@ class CircuitTranspiler:
         # your code here
         return {}
 
-    def routing(timesteps: List[List[Tuple[int, int]]], initial_mapping: Dict[int, int]) -> models.Circuit:
+    def routing(self, timesteps: List[List[Tuple[int, int]]], initial_mapping: Dict[int, int]) -> models.Circuit:
         """
         Function that takes as input the timesteps and the initial mapping and outputs the final circuit.
 
@@ -53,7 +62,35 @@ class CircuitTranspiler:
         Returns:
         models.Circuit: A Qibo circuit object representing the final quantum circuit after applying the routing algorithm.
         """
-        return models.Circuit(5)
+        # (name, (qubit1, qubit2))
+        # Run over all timesteps and gates in the timesteps
+        for timestep in timesteps:
+            for gate in timestep:
+
+                # If the gate is a single qubit gate, add it to the output circuit following the mapping
+                if len(gate[2]) == 1:
+                    self.output_circuit.add(string_gate(gate[0], self.mapping[gate[1][0]]))
+
+                # If the gate is a two qubit gate, check if the qubits are connected in the topology
+                elif len(gate[2]) == 2:
+                    if (self.mapping[gate[1][0]], self.mapping[gate[1][1]]) in list(self.topology_graph.edges):
+                        self.output_circuit.add(
+                            string_gate(gate[0], (self.mapping[gate[1][0]], self.mapping[gate[1][1]]))
+                        )
+                    else:
+                        path = astar_path(self.topology_graph, self.mapping[gate[1][0]], self.mapping[gate[1][1]])
+
+                        for i in range(len(path) - 1):
+                            self.output_circuit.add(string_gate("CNOT", (path[i], path[i + 1])))
+                            self.output_circuit.add(string_gate("CNOT", (path[i + 1], path[i])))
+                            self.output_circuit.add(string_gate("CNOT", (path[i], path[i + 1])))
+
+                        self.mapping[gate[2][1]] = gate[2][0]
+                        self.mapping[gate[2][0]] = gate[2][1]
+
+                        self.output_circuit.add(string_gate(gate[0], (path[-1], self.mapping[gate[2][0]])))
+
+        return self.output_circuit
 
     def optimize_circuit(circuit: models.Circuit) -> models.Circuit:
         """
@@ -66,3 +103,43 @@ class CircuitTranspiler:
         models.Circuit: The optimized circuit.
         """
         return circuit
+
+
+def string_gate(name: str, qubits: int | tuple[int, int]) -> gates.Gate:
+    """Converts a tuple representation of a get as (name, qubits) into a Gate object.
+
+    Args:
+        name (str): The name of the gate. Can be "cnot", "x" or "h".
+        qubits (int | tuple[int, int]): The qubits the gate acts on. If the gate is a single qubit gate, it is an int.
+        If it is a two qubit gate, it is a tuple of two ints.
+
+    Raises:
+        ValueError: If the gate name is not supported.
+
+    Returns:
+        gates.Gate: The qibo Gate object.
+    """
+    if name == "cnot":
+        return gates.CNOT(*qubits)
+    elif name == "x":
+        return gates.X(qubits)
+    elif name == "h":
+        return gates.H(qubits)
+    else:
+        raise ValueError(f"Gate {name} not supported")
+
+
+def dict_topology_tolist(topology: dict[int, list[int]]) -> List[Tuple[int, int]]:
+    """Helper function to convert a dictionary {node: [neighbors]} into a list of edges as tuples of nodes.
+
+    Args:
+        topology (dict[int, list[int]]): The topology of the quantum device represented as a dictionary {node: [neighbors]}.
+
+    Returns:
+        List[Tuple[int, int]]: The list of edges as tuples of nodes.
+    """
+    edges = []
+    for node, neighbors in topology.items():
+        for neighbor in neighbors:
+            edges.append((node, neighbor))
+    return edges
